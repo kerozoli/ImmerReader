0fffd3a6da214ffad0374ebda5ac1ba4f9f6d47c
Zoltán Keresztes <kerozoli@gmail.com>
2025-05-10 13:08:02 +0200

    AristonController working

---
+++ src/main/java/com/keroleap/immerreader/AristonRest.java
@@ -0,0 +1,16 @@
+package com.keroleap.immerreader;
+
+public class AristonRest {
+    private int percentage;
+
+
+
+    public int getPeecentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+
+}
