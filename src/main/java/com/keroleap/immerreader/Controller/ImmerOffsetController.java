99725de6164b9749fed75018d8dfd003167bad0d
kerozoli <kerozoli@gmail.com>
2026-04-16 20:37:40 +0200

    Add web UI for offset adjustment with live image preview
    - New GET /ImmerOffset/adjust endpoint with HTML form
    - Thymeleaf template with X/Y offset inputs and submit button
    - Live camera feed preview from /Immer/uncroppedimage
    - Auto-refresh image every 2 seconds
    - AJAX form submission with visual feedback

--- src/main/java/com/keroleap/immerreader/Controller/ImmerOffsetController.java
+++ src/main/java/com/keroleap/immerreader/Controller/ImmerOffsetController.java
@@ -1,15 +1,17 @@
 package com.keroleap.immerreader.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.keroleap.immerreader.SharedData.ImmerOffsetData;
 
-@RestController
+@Controller
 @RequestMapping("/ImmerOffset")
 public class ImmerOffsetController {
 
@@ -17,6 +19,7 @@ public class ImmerOffsetController {
     private ImmerOffsetData immerOffsetData;
 
     @PostMapping("/set")
+    @ResponseBody
     public ImmerOffsetData setOffset(@RequestParam int x, @RequestParam int y) {
         immerOffsetData.setOffsetX(x);
         immerOffsetData.setOffsetY(y);
@@ -24,7 +27,16 @@ public class ImmerOffsetController {
     }
 
     @GetMapping
+    @ResponseBody
     public ImmerOffsetData getOffset() {
         return immerOffsetData;
+    }
+
+    @GetMapping("/adjust")
+    public ModelAndView adjustOffset() {
+        ModelAndView modelAndView = new ModelAndView("offset-adjuster");
+        modelAndView.addObject("offsetX", immerOffsetData.getOffsetX());
+        modelAndView.addObject("offsetY", immerOffsetData.getOffsetY());
+        return modelAndView;
     }
 }
